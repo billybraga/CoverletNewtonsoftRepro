e39ecf3a3a0ca31186d482dda0968bcae6b2fbbf
Billy Braga <bbraga@progi.com>
2021-08-09 09:35:11 -0400

    first commit

---
+++ CoverletNewtonsoftRepro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CoverletNewtonsoftRepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(new { prop = "value" }));
+        }
+    }
+}
