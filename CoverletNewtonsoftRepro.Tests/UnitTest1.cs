4dee0c3eba16335ce02783dd1cb846cfd5bd7370
Billy Braga <bbraga@progi.com>
2021-08-09 12:28:08 -0400

    Update to remove coverlet dependency from console project.

---
+++ CoverletNewtonsoftRepro.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace CoverletNewtonsoftRepro.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
